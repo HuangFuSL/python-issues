35757e6f3607b3247b41cf3fa78e560de4a094e7
HuangFuSL <56627153+HuangFuSL@users.noreply.github.com>
2021-03-10 11:25:52 +0800

    Decorator refactor and added argument parsers

--- pyissues/__main__.py
+++ pyissues/__main__.py
@@ -9,7 +9,7 @@ import operator
 import os
 import sys
 import time
-from typing import Any, Dict, Iterable, List, Set
+from typing import Any, Callable, Dict, Iterable, List, Set
 
 from . import base as base
 from . import const as const
@@ -17,7 +17,16 @@ from . import io as issuesIO
 from . import network as network
 
 
-def is_compressed(_): return _[-2:] == "gz"
+sub_commands: Dict[str, Callable] = {}
+
+
+def sub_command(o: Callable) -> Callable:
+    sub_commands[o.__name__] = o
+    return o
+
+
+def is_compressed(_: str) -> str:
+    return _[-2:] == "gz"
 
 
 def _numbering(start: int = 1) -> str:
@@ -51,12 +60,13 @@ def reshape_meta(o: Dict[int | str, int]) -> List[Set[int | str]]:
 
 
 def update_meta(
-    meta: Dict[int, int], path: str | io.IOBase = "meta.json") -> None:
+        meta: Dict[int, int], path: str | io.IOBase = "meta.json") -> None:
     if isinstance(path, str) and path:
         with open(path, "w", encoding="utf-8") as file:
             json.dump(meta, file)
     elif isinstance(path, io.IOBase):
         json.dump(meta, path)
+
 
 def refresh_meta(
     new: Dict[int, int],
@@ -77,7 +87,7 @@ def refresh_meta(
     return compare_meta(old, new, fullupdate)
 
 
-def fetch(records: Iterable[int], threads: int = 16) -> List[base.Issue]:
+def fetch(records: Iterable[int], threads: int | None = 16) -> List[base.Issue]:
     print("Fetching %d issues from bugs.python.org" % (len(records), ))
     with multiprocessing.Pool(threads) as pool:
         start = time.time()
@@ -91,13 +101,97 @@ def fetch(records: Iterable[int], threads: int = 16) -> List[base.Issue]:
 
 
 def write(
-    obj: List[base.Issue], path: str = "issues.xml.gz") -> List[base.Issue]:
+    obj: List[base.Issue], path: str = "issues.xml.gz"
+) -> List[base.Issue]:
     if is_compressed(path):
         issuesIO.xmldumpCompressed(obj, path)
     else:
         issuesIO.xmldump(obj, path)
     print("%d issues written to %s" % (len(obj), path))
     return obj
+
+
+@sub_command
+def rebuild(
+    *, metafile: str = "meta.json", threads: int | None = None, **kwargs):
+    print("Fetching list.")
+    new_list = network.get_list()
+    update = set()
+    with open(metafile, "w+") as file:
+        json.dump({}, file)
+        file.seek(0)
+        update = refresh_meta(new_list, file)
+        json.dump(new_list, file)
+    print("Fetching issues.")
+    return write(fetch(update, threads))
+
+
+@sub_command
+def refetch(
+    *, metafile: str = "meta.json", threads: int | None = None, **kwargs):
+    with open(metafile, "r") as file:
+        update = refresh_meta(json.load(file), {})
+    print("%d issues loaded." % (len(update), ))
+    print("Fetching issues.")
+    return write(fetch(update, threads))
+
+
+@sub_command
+def check(*, metafile: str = "meta.json", **kwargs):
+    print("Fetching list.")
+    new_list = network.get_list()
+    update = set()
+    with open(metafile, "r") as file:
+        update = refresh_meta(new_list, file)
+    return update
+
+
+@sub_command
+def fix(*, 
+    metafile: str = "meta.json",
+    datafile: str = "issues.xml.gz",
+    threads: int | None = None,
+    **kwargs
+):
+    print("Loading list,")
+    with open(metafile, "r") as file:
+        update = refresh_meta(json.load(file), {})
+    print("Loading issues.")
+    issues = issuesIO.xmlloadCompressed(datafile)
+    issuesID = {int(_._id) for _ in issues}
+    print("%d issues not fetched." % (len(update - issuesID), ))
+    issues.extend(fetch(update - issuesID, threads))
+    return write(issues)
+
+
+@sub_command
+def update(*,
+    fullupdate: bool = False,
+    metafile: str = "meta.json",
+    datafile: str = "issues.xml.gz",
+    threads: int | None = None, **kwargs):
+    print("Loading list.")
+    new_list = network.get_list()
+    update = refresh_meta(new_list, metafile, fullupdate=fullupdate)
+    if update:
+        print("Loading issues.")
+        issues = issuesIO.xmlloadCompressed(datafile, dict)
+        new_issues = fetch(update, threads)
+        for _ in new_issues:
+            issues[int(_._id)] = _
+        update_meta(new_list)
+        write(issues)
+    else:
+        print("No change detected.")
+        return None
+
+
+@sub_command
+def load(*, datafile: str = "issues.xml.gz", **kwargs):
+    print("Loading issues.")
+    issues = issuesIO.xmlloadCompressed(datafile, dict)
+    print("Loaded issues are saved in `ret`")
+    return issues
 
 
 def main(*args) -> Any:
@@ -109,56 +203,26 @@ def main(*args) -> Any:
     if not os.path.exists(os.path.join(user_root, ".pyissues")):
         os.mkdir(config_path)
     os.chdir(config_path)
+
+    parser = argparse.ArgumentParser(description="Python issues tool")
+    parser.add_argument(
+        '--fullupdate', '-fu',
+        action='store_const', dest='fullupdate', const=True, default=None)
+    parser.add_argument(
+        '--threads', '-t',
+        nargs='?', dest='threads', default=None)
+    parser.add_argument(
+        '--meta', '-m',
+        nargs='?', dest='metafile', default='meta.json')
+    parser.add_argument(
+        '--data', '-d',
+        nargs='?', dest='datafile', default='issues.xml.gz')
+
     if not args:
         return
-    if args[0] == "rebuild":
-        print("Fetching list.")
-        new_list = network.get_list()
-        update = set()
-        with open("meta.json", "w+") as file:
-            json.dump({}, file)
-            file.seek(0)
-            update = refresh_meta(new_list, file)
-            json.dump(new_list, file)
-        print("Fetching issues.")
-        return write(fetch(update, 64))
-    elif args[0] == "refetch":
-        with open("meta.json", "r") as file:
-            update = refresh_meta(json.load(file), {})
-        print("%d issues loaded." % (len(update), ))
-        print("Fetching issues.")
-        return write(fetch(update, 64))
-    elif args[0] == "check":
-        print("Fetching list.")
-        new_list = network.get_list()
-        update = set()
-        with open("meta.json", "r") as file:
-            update = refresh_meta(new_list, file)
-        return update
-    elif args[0] == "fix":
-        print("Loading list,")
-        with open("meta.json", "r") as file:
-            update = refresh_meta(json.load(file), {})
-        print("Loading issues.")
-        issues = issuesIO.xmlloadCompressed("issues.xml.gz")
-        issuesID = {int(_._id) for _ in issues}
-        print("%d issues not fetched." % (len(update - issuesID), ))
-        issues.extend(fetch(update - issuesID))
-        return write(issues)
-    elif args[0] == "update":
-        print("Loading list.")
-        new_list = network.get_list()
-        update = refresh_meta(new_list, fullupdate=True)
-        if update:
-            print("Loading issues.")
-            issues = issuesIO.xmlloadCompressed("issues.xml.gz", dict)
-            new_issues = fetch(update, 32)
-            for _ in new_issues:
-                issues[int(_._id)] = _
-            update_meta(new_list)
-            write(issues)
-        else:
-            print("No change detected.")
+    else:
+        return sub_commands[args[0]](**vars(parser.parse_args(args[1:])))
+
 
 if __name__ == "__main__":
     if len(sys.argv) >= 2:
